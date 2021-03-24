e08acd1ca99ab8ca646c02228e5bc6de478346f5
Marin Aglic Cuvic <mrn.aglic@gmail.com>
2021-03-24 12:03:57 +0100

    Added AkkaConfigProvider and example project

---
+++ AkkaConfigExample/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Akka.Actor;
+using Akka.Routing;
+using AkkaConfigProvider;
+
+namespace AkkaConfigExample
+{
+    class Print {}
+
+    class MyActor : ReceiveActor
+    {
+        public MyActor()
+        {
+            Receive<Print>(x => Console.WriteLine(Self.Path));
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var configProvider = new ConfigProvider();
+            var config = configProvider.GetAkkaConfig<MyAkkaConfig>();
+            
+            using (var system = ActorSystem.Create("config-example", config))
+            {
+                var props = Props.Create(() => new MyActor())
+                    .WithRouter(FromConfig.Instance);
+                var router = system.ActorOf(props, "my-router");
+                
+                router.Tell(new Print());
+                router.Tell(new Print());
+                router.Tell(new Print());
+                router.Tell(new Print());
+                router.Tell(new Print());
+
+                system.Terminate().Wait();
+            }
+        }
+    }
+}
